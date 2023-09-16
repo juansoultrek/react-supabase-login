2fcc0f19cfce518da70a116eeb176df87e4eabb4
Juan Villegas <juancarlos.villegas@gmail.com>
2023-09-16 14:56:50 -0600

    Clean app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import  { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./pages/loginPage";
+import Success from "./pages/successPage";
+
 
 function App() {
+  console.log(process.env.REACT_APP_SUPABASE_CLIENT_URL);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/success" element={<Success/>} />
+        </Routes>
+      </Router>
   );
 }
 
