c2d1891a0363c693cad501ef42ed616ba34c8325
Juan Villegas <juancarlos.villegas@gmail.com>
2023-09-16 16:18:49 -0600

    Setup the supabas client auth

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,9 +1,31 @@
+import {createClient} from "@supabase/supabase-js";
+import {useNavigate} from "react-router-dom";
+import {Auth} from "@supabase/auth-ui-react";
+import {ThemeSupa,} from '@supabase/auth-ui-shared'
 
+
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_CLIENT_URL,
+    process.env.REACT_APP_SUPABASE_PROJECT_KEY
+)
 function Login() {
+    const navigate = useNavigate();
+    supabase.auth.onAuthStateChange(async (event) =>{
+        console.log('Auth state changed:', event);
+        if (event === "SIGNED OUT") {
+            navigate("/success");
+        } else {
+            navigate("/");
+        }
+    })
     return (
         <div className="App">
             <header className="App-header">
-
+                <Auth
+                    supabaseClient={supabase}
+                    appearance={{ theme: ThemeSupa }}
+                    providers={['discord']}
+                />
             </header>
         </div>
     );
