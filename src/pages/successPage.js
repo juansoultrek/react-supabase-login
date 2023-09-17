fffdfa4d659012f1ce6aa2710f0d4674795414e1
Juan Villegas <juancarlos.villegas@gmail.com>
2023-09-17 08:03:10 -0600

    Set user when logged in

--- src/pages/successPage.js
+++ src/pages/successPage.js
@@ -1,5 +1,32 @@
+import {createClient} from "@supabase/supabase-js";
+import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState} from "react";
 
+
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_CLIENT_URL,
+    process.env.REACT_APP_SUPABASE_PROJECT_KEY
+)
 function Success() {
+
+    const [user, setUser] = useState({});
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        async function getUserData(){
+            await supabase.auth.getUser().then((value) => {
+                if(value.data?.user){
+                    console.log(value.data.user)
+                    setUser(value.data.user);
+                }
+
+            })
+        }
+        getUserData();
+    }, []);
+
+
+
     return (
         <div className="App">
             <header className="App-header">
